4a49eb02d00a2eb94652025f1187939b84ab0ae0
Ben Wu <wubin28@gmail.com>
2014-11-04 13:28:08 +0800

    Added test 'a val can never be reassigned but a var can'.

--- src/test/scala/KatasScalaSuite.scala
+++ src/test/scala/KatasScalaSuite.scala
@@ -8,6 +8,17 @@ class KatasScalaSuite extends FunSuite{
     val douglas = new KatasScala
     assert(douglas.answer() === (42))
   }
+
+  test("a val can never be reassigned but a var can") {
+//    val msg = "Hello, world!"
+//    msg = "Goodbye cruel world!" // Got "error: reassignment to val"
+//    assert(msg === "Goodbye cruel world!")
+
+    var anotherMsg = "Hello, world!"
+    anotherMsg = "Goodbye cruel world!"
+    assert(anotherMsg === "Goodbye cruel world!")
+
+  }
 }
 
 
