8717c0a16f9685d91d95df780f5b31b61823af86
Ben Wu <wubin28@gmail.com>
2014-11-15 21:15:51 +0800

    Added a trivial test for test class FuncProgInScalaCh01.

--- src/test/scala/FuncProgInScalaCh01.scala
+++ src/test/scala/FuncProgInScalaCh01.scala
@@ -1,6 +1,10 @@
+import org.scalatest.FunSuite
+
 /**
  * Created by ben on 11/15/14.
  */
-class FuncProgInScalaCh01 {
-
+class FuncProgInScalaCh01 extends FunSuite{
+  test("a trivial test") {
+    assert(2 + 3 === 5)
+  }
 }
