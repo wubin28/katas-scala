cd3f4b0e263c27178c8c55832408161fbf239055
Ben Wu <wubin28@gmail.com>
2014-11-16 19:45:02 +0800

    Wrote intention code for Assert part of test 'a simple scala program'.

---
+++ src/test/scala/FuncProgInScalaCh02.scala
@@ -0,0 +1,11 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created by ben on 11/15/14.
+ */
+class FuncProgInScalaCh02 extends FunSuite{
+  test("a simple scala program p15") {
+    // Assert
+    assert(myAbs.formatAbs(-42) === "The absolute value of -42 is 42")
+  }
+}
