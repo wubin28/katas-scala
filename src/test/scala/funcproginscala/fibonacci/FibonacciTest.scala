d2e552a87ec0719d9b4ada406bc18257fb9b81b5
Ben Wu <wubin28@gmail.com>
2014-11-17 21:24:27 +0800

    Fixed Error: Implemented method fibonacci().

--- src/test/scala/funcproginscala/fibonacci/FibonacciTest.scala
+++ src/test/scala/funcproginscala/fibonacci/FibonacciTest.scala
@@ -6,7 +6,16 @@ import org.scalatest.FunSuite
  * Created by ben on 11/17/14.
  */
 class FibonacciTest extends FunSuite{
+
+
   test("the 7th fibonacci number is 13") {
+    def fibonacci(n: Int): Int = {
+      def go(n: Int, a: Int, b: Int): Int = n match {
+        case 0 => a
+        case _ => go(n-1, b, a+b)
+      }
+      go(n, 0, 1)
+    }
     assert(fibonacci(7) === 13)
   }
 }
