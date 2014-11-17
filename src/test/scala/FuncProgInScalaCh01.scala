aab98dc9797a734e760ab525d25651062437d34b
Ben Wu <wubin28@gmail.com>
2014-11-17 11:03:15 +0800

    Fixed Error: Created class Cafe.

--- src/test/scala/FuncProgInScalaCh01.scala
+++ src/test/scala/FuncProgInScalaCh01.scala
@@ -6,8 +6,10 @@ import org.scalatest.FunSuite
 class FuncProgInScalaCh01 extends FunSuite{
 
 
+
   test("the result of function buyCoffee() of class Cafe should be a Coffee with side effects p4") {
     val cc = new CreditCard
+    val cafe = new Cafe
 
     // Assert
     assert(cafe.buyCoffee(cc).getClass === (new Coffee).getClass)
