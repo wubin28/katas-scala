5b02ecaaff8e67256a40c5495c2a2f0deef3cd1c
Ben Wu <wubin28@gmail.com>
2014-11-17 20:22:53 +0800

    Fixed Error: Created and implemented method factorial().

--- src/test/scala/funcproginscala/factorial/FactorialTest.scala
+++ src/test/scala/funcproginscala/factorial/FactorialTest.scala
@@ -6,7 +6,16 @@ import org.scalatest.FunSuite
  * Created by ben on 11/17/14.
  */
 class FactorialTest extends FunSuite{
-  test("the factorial of 5 is 120 p20") {
+
+  test("writing loops functionally: the factorial of 5 is 120 p20") {
+    def factorial(n: Int): Int = {
+      def go(n: Int, acc: Int): Int =
+        if (n <= 0) acc
+        else go(n-1, n*acc)
+
+      go(n, 1)
+    }
+
     assert(factorial(5) === 120)
   }
 }
