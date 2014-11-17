920321684c4a3b8231b66dc222b86b7e6ec4c374
Ben Wu <wubin28@gmail.com>
2014-11-17 21:46:30 +0800

    Tested the 3rd fibonacci number.

--- src/test/scala/funcproginscala/fibonacci/FibonacciTest.scala
+++ src/test/scala/funcproginscala/fibonacci/FibonacciTest.scala
@@ -6,16 +6,15 @@ import org.scalatest.FunSuite
  * Created by ben on 11/17/14.
  */
 class FibonacciTest extends FunSuite{
-
-
-  test("the 7th fibonacci number is 13") {
+  test("the 3th fibonacci number is 2") {
     def fibonacci(n: Int): Int = {
+      @annotation.tailrec
       def go(n: Int, a: Int, b: Int): Int = n match {
         case 0 => a
         case _ => go(n-1, b, a+b)
       }
       go(n, 0, 1)
     }
-    assert(fibonacci(7) === 13)
+    assert(fibonacci(3) === 2)
   }
 }
