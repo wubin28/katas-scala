7d8daf016f0578bba05e2e0b683f980910711fd5
Ben Wu <wubin28@gmail.com>
2014-11-17 10:55:40 +0800

    Added Assert part of test 'the result of function buyCoffee() of class Cafe should be a Coffee with side effects p4'.

---
+++ src/test/scala/FuncProgInScalaCh01.scala
@@ -0,0 +1,11 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created by ben on 11/17/14.
+ */
+class FuncProgInScalaCh01 extends FunSuite{
+  test("the result of function buyCoffee() of class Cafe should be a Coffee with side effects p4") {
+    // Assert
+    assert(cafe.buyCoffee(cc).getClass === (new Coffee).getClass)
+  }
+}
