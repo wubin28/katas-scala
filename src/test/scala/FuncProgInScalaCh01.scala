fdad3a49f2622298db9c2f81ec8a906b98208ee2
Ben Wu <wubin28@gmail.com>
2014-11-17 11:01:42 +0800

    Fixed Error: Created class CreditCard.

--- src/test/scala/FuncProgInScalaCh01.scala
+++ src/test/scala/FuncProgInScalaCh01.scala
@@ -4,7 +4,11 @@ import org.scalatest.FunSuite
  * Created by ben on 11/17/14.
  */
 class FuncProgInScalaCh01 extends FunSuite{
+
+
   test("the result of function buyCoffee() of class Cafe should be a Coffee with side effects p4") {
+    val cc = new CreditCard
+
     // Assert
     assert(cafe.buyCoffee(cc).getClass === (new Coffee).getClass)
   }
