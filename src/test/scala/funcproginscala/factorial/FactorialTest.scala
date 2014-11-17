9d4363fbfac93119e261adb9e6645876f1c19fdc
Ben Wu <wubin28@gmail.com>
2014-11-17 20:44:12 +0800

    Added @annotation.tailrec.

--- src/test/scala/funcproginscala/factorial/FactorialTest.scala
+++ src/test/scala/funcproginscala/factorial/FactorialTest.scala
@@ -9,6 +9,7 @@ class FactorialTest extends FunSuite{
 
   test("writing loops functionally: the factorial of 5 is 120 p20") {
     def factorial(n: Int): Int = {
+      @annotation.tailrec
       def go(n: Int, acc: Int): Int =
         if (n <= 0) acc
         else go(n-1, n*acc)
