a4e192e038dc2ac101dbf987f02c5781267ba98b
Ben Wu <wubin28@gmail.com>
2014-11-19 09:14:05 +0800

    Wrote assert part of test 'K: polymorphic function to find an element in an array p23'.

--- src/test/scala/funcproginscala/PolymorphicFunction.scala
+++ src/test/scala/funcproginscala/PolymorphicFunction.scala
@@ -8,12 +8,16 @@ import org.scalatest.FunSuite
 class PolymorphicFunction extends FunSuite{
 
   test("K: monomorphic function to find a string in an array p23") {
-    def findFirst(ss: Array[String], key: String) = {
+    def findFirst(ss: Array[String], key: String): Int = {
       @annotation.tailrec
       def loop(n: Int): Int =
         if (n >= ss.length) -1 else if (ss(n) == key) n else loop(n + 1)
       loop(0)
     }
     assert(findFirst(Array("Susan", "Ben", "Louis"), "Ben") === 1)
+  }
+
+  test("K: polymorphic function to find an element in an array p23") {
+    assert(findFirst(Array(6, 9, 5, 7), (x: Int) => x == 5) === 2)
   }
 }
