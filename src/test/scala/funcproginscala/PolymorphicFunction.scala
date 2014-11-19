c2273e0d1e214ef08c259fa7454c4a964e09d5f2
Ben Wu <wubin28@gmail.com>
2014-11-19 10:14:19 +0800

    Fixed Error: Implemented method isSorted[T]().

--- src/test/scala/funcproginscala/PolymorphicFunction.scala
+++ src/test/scala/funcproginscala/PolymorphicFunction.scala
@@ -28,7 +28,13 @@ class PolymorphicFunction extends FunSuite{
   }
 
   test("Exercise 2.2: Implement isSorted which checks whether an Array[A] is sorted according to a given comparison function p24") {
-    def isSorted[T](as: Array[T], ordered: (T, T) => Boolean): Boolean = {}
+    def isSorted[T](as: Array[T], ordered: (T, T) => Boolean): Boolean = {
+      if (as.length <= 1) return true
+      def loop(n: Int): Boolean = {
+        if (n >= as.length) true else if (!ordered(as(n-1), as(n))) false else loop(n + 1)
+      }
+      loop(1)
+    }
     assert(isSorted(Array(6, 9, 5, 7), (x: Int, y: Int) => x < y) === false)
     assert(isSorted(Array(5, 6, 7, 9), (x: Int, y: Int) => x < y) === true)
   }
