2b6c4ebbc387b98ba5e1241636b17cdf3458db0b
Ben Wu <wubin28@gmail.com>
2014-12-14 09:41:04 +0800

    Added knowledge comments to test P23_Polymorphic_Function.

--- src/test/scala/funcproginscala/P23_Polymorphic_Function.scala
+++ src/test/scala/funcproginscala/P23_Polymorphic_Function.scala
@@ -7,6 +7,12 @@ import org.scalatest.FunSuite
  */
 class P23_Polymorphic_Function extends FunSuite{
 
+/*
+  The following monomorphic function, findFirst, returns the first index in an
+  array where the key occurs, or -1 if it’s not found. It’s specialized for
+  searching for a String in an Array of String values.
+*/
+
   test("K: monomorphic function to find a string in an array p23") {
     def findFirst(ss: Array[String], key: String): Int = {
       @annotation.tailrec
@@ -17,6 +23,28 @@ class P23_Polymorphic_Function extends FunSuite{
     assert(findFirst(Array("Susan", "Ben", "Louis"), "Ben") === 1)
   }
 
+/*
+  This is an example of a polymorphic function, sometimes called a generic function.
+  We’re abstracting over the type of the array and the function used for searching it.
+  To write a polymorphic function as a method, we introduce a comma-separated list of
+  type parameters, surrounded by square brackets (here, just a single [A]), following
+  the name of the function, in this case findFirst. We can call the type parameters
+  anything we want—[Foo, Bar, Baz] and [TheParameter, another_good_one] are valid
+  type parameter declarations—though by convention we typically use short, one-letter,
+  uppercase type parameter names like [A,B,C].
+
+  The type parameter list introduces type variables that can be referenced in the
+  rest of the type signature (exactly analogous to how variables introduced in the
+  parameter list to a function can be referenced in the body of the function). In
+  findFirst, the type variable A is referenced in two places: the elements of the
+  array are required to have the type A (since it’s an Array[A]), and the p function
+  must accept a value of type A (since it’s a function of type A => Boolean).
+  The fact that the same type variable is referenced in both places in the type
+  signature implies that the type must be the same for both arguments, and the compiler
+  will enforce this fact anywhere we try to call findFirst. If we try to
+  search for a String in an Array[Int], for instance, we’ll get a type mismatch error.
+
+*/
   test("K: polymorphic function to find an element in an array p23") {
     def findFirst[T](as: Array[T], p: (T) => Boolean): Int = {
       @annotation.tailrec
